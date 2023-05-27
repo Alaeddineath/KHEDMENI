1467ac0b275bd53af700590d9df6333e9df253c5
ZakariaBoukeffa <125987147+ZakariaBoukeffa@users.noreply.github.com>
2023-05-27 21:41:35 +0100

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE khademni;
 
+CREATE DATABASE khademni;
+
 CREATE TABLE users (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -7,6 +9,8 @@ CREATE TABLE users (
     phone VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    about varchar(1000) ,
+    Job varchar(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -14,8 +18,10 @@ CREATE TABLE companies (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     company_name VARCHAR(100) NOT NULL,
     head_name VARCHAR(100) NOT NULL,
-    address TEXT NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL   
+    about varchar(1000) ,
+    field varchar(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE jobs (
@@ -25,7 +31,8 @@ CREATE TABLE jobs (
     job_type VARCHAR(255),
     job_salary float,
     job_id INT PRIMARY KEY AUTO_INCREMENT,
-    worker_type VARCHAR(255)
+    worker_type VARCHAR(255),
+    job_poster INT(11) 
 );
 
 CREATE TABLE comments (
